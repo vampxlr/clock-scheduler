0b4438c2ea289c096bef7f3464bf992e86f7f2b5
vampxlr <vampxlr@yahoo.com>
2016-07-05 20:32:12 +0600

    add pie to circle

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,34 @@ var ctx = (canvas.getContext) ?
 
 if(ctx){
 
-    ctx.fillStyle = '#28d1fa';
+    ctx.fillStyle = '#4A4464';
     ctx.lineWidth = 17;
     ctx.lineCap = 'round';
     ctx.shadowBlur = 15;
-    ctx.shadowColor = '#28d1fa';
+    ctx.shadowColor = '#fff';
 
     function degToRad(degree) {
         var factor = Math.PI/180;
         return degree*factor;
     }
 
+    var centerX = Math.floor(canvas.width / 2);
+    var centerY = Math.floor(canvas.height / 2);
+    console.log("centerX: "+centerX)
+    console.log("centerY: "+centerY)
+
+        var data = [360 ];
+        var labels = ["360"];
+        var colors = ["#733EE3"];
 
 
-        var data = [90, 100, 140];
-        var labels = ["90", "100", "140"];
-        var colors = ["#FFDAB9", "#E6E6FA", "#E0FFFF"];
+
 
     function drawSegment(canvas, context, i) {
         context.save();
         var centerX = Math.floor(canvas.width / 2);
         var centerY = Math.floor(canvas.height / 2);
-        radius = Math.floor(canvas.width / 3);
+        radius = Math.floor(canvas.width / 2);
 
         var startingAngle = degreesToRadians(sumTo(data, i));
         var arcSize = degreesToRadians(data[i]);
@@ -47,8 +53,37 @@ if(ctx){
         drawSegmentLabel(canvas, context, i);
     }
 
+
+    function drawSegmentWithLocation(canvas, context, degstartingAngle) {
+        context.save();
+        var centerX = Math.floor(canvas.width / 2);
+        var centerY = Math.floor(canvas.height / 2);
+        radius = Math.floor(canvas.width / 2);
+
+        var startingAngle = degreesToRadians(degstartingAngle);
+        var arcSize = degreesToRadians(30);
+        var endingAngle = startingAngle + arcSize;
+
+        context.beginPath();
+        context.moveTo(centerX, centerY);
+        context.arc(centerX, centerY, radius,
+            startingAngle, endingAngle, false);
+        context.closePath();
+
+        context.fillStyle = "#fff";
+        context.fill();
+
+        context.restore();
+
+        //drawSegmentLabel(canvas, context, i);
+    }
+
+
     function degreesToRadians(degrees) {
         return (degrees * Math.PI)/180;
+    }
+    function radiansToDegrees(rad) {
+        return (rad / Math.PI)*180;
     }
     function sumTo(a, i) {
         var sum = 0;
@@ -79,11 +114,61 @@ if(ctx){
         context.restore();
     }
 
+    function pixelToDegree(x,y){
+    var delY = y - centerY
+    var delX = x - centerX
+    var quardant = 1;
+        if(delY>0 && delX>0)
+        {
+            quardant = 1
+        }
+
+        if(delY>0 && delX<0)
+        {
+            quardant = 2
+        }
+        if(delY<0 && delX<0)
+        {
+            quardant = 3
+        }
+        if(delY<0 && delX>0)
+        {
+            quardant = 4
+        }
+
+
+    var gradient = delY/delX
+        radSlope = Math.atan(gradient)
+        degrees = radiansToDegrees(radSlope)
+        console.log(degrees)
+        console.log(quardant)
+        return degrees
+    }
+
     for (var i = 0; i < data.length; i++) {
         drawSegment(canvas, ctx, i);
     }
 
+    drawSegmentWithLocation(canvas,ctx,20)
+    pixelToDegree(251,241)
 
+
+    function clickdraw(){
+      var  x = event.offsetX;
+
+       var y = event.offsetY;
+
+    console.log("x: "+x)
+    console.log("y: "+y)
+        console.log(event)
+       console.log( pixelToDegree(x,y))
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawSegment(canvas, ctx, 0);
+        drawSegmentWithLocation(canvas,ctx,pixelToDegree(x,y)-15)
+
+    }
+
+    canvas.addEventListener('mousemove', clickdraw);
 
 }
 console.log(ctx)
